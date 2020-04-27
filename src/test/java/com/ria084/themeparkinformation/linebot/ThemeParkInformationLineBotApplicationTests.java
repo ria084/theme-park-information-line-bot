c3561aafb8bea3f3cb6e726c38c95f2f90a4e536
Iria <siki.fate18.cat@gmail.com>
2020-04-27 16:28:18 +0900

    gredle追加

---
+++ src/test/java/com/ria084/themeparkinformation/linebot/ThemeParkInformationLineBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ria084.themeparkinformation.linebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThemeParkInformationLineBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
