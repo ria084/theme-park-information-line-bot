59c26739d75ba07f67f65356f4470fba80affd17
Iria <siki.fate18.cat@gmail.com>
2020-04-27 17:39:55 +0900

    基本機能実装

--- src/main/java/com/ria084/themeparkinformation/linebot/ThemeParkInformationLineBotApplication.java
+++ src/main/java/com/ria084/themeparkinformation/linebot/ThemeParkInformationLineBotApplication.java
@@ -1,13 +1,30 @@
 package com.ria084.themeparkinformation.linebot;
 
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@LineMessageHandler
 public class ThemeParkInformationLineBotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ThemeParkInformationLineBotApplication.class, args);
 	}
 
+	@EventMapping
+	public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+		System.out.println("event: " + event);
+		return new TextMessage(event.getMessage().getText());
+	}
+
+	@EventMapping
+	public void handleDefaultMessageEvent(Event event) {
+		System.out.println("event: " + event);
+	}
 }
